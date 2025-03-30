947c60ab2382d3d821ec902dc804e005c8677f11
dhvcc <alexey.artishevskiy@gmail.com>
2025-03-30 22:07:45 +0200

    Rewrite lampac cringe

--- plugins/lampa-stack.js
+++ plugins/lampa-stack.js
@@ -100,7 +100,7 @@
       author: "@lme_chat",
     },
     {
-      url: "/plugins/patched/lampac-online.js",
+      url: "/lampac-api/lampac-api/online.js",
       status: 1,
       name: "Lampac Online",
       author: "@immisterio",
